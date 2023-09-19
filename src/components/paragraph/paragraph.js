531097ba3617e3e69b6980689dce521566bfdf69
Vitor Johann <johannvitorhugoojeda@gmail.com>
2023-09-19 18:38:30 -0300

    first commit

---
+++ src/components/paragraph/paragraph.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Paragraph = () => {
+    return <p style={{ color: "lightcoral", textTransform: "uppercase" }} >Lee, oh Lee, o que foi que eu fiz? Olha só para você, nem está consciente e continua se empenhando em mostrar para o mundo o que pode fazer. Ele está nocauteado, tudo que o mantém de pé é o poder de sua vontade.</p>
+}
+
+export default Paragraph;
